4d7b3970d5e65b115f4bcc5f62258c0fde3b1fea
alleycaaat <achulslander@gmail.com>
2023-02-03 13:01:34 -0600

    reset FoodNutri when no food is selected

--- src/components/AddFuel.js
+++ src/components/AddFuel.js
@@ -1,46 +1,143 @@
-import { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
-const AddFuel = ({
-    h20,
-    tailwind,
-    fuel,
-    times,
-    save,
-    onlyWater,
-    setMessage,
-}) => {
+import { HourContext } from '../store/hour-context';
+import { FuelContext } from '../store/fuel-context';
+
+import { SelectFuel, SelectML, SelectServings, SelectTime } from './addFuel/Select';
+import { edit, getFuel, gethours } from '../util/api';
+import { GetItemId } from '../util/GetItemId';
+import { Constants } from '../constants/Constants';
+import { BlockServ } from './charts/Blocks';
+
+import WaterHandler from '../util/WaterHandler';
+import TailwindHandler from '../util/TailwindHandler';
+import FoodHandler from '../util/FoodHandler';
+import Chart from './charts/Chart';
+
+const AddFuel = ({ setLoading, setMessage }) => {
+
+    const { fuel, setFuel } = useContext(FuelContext);
+    const { hours, setHours, addToHour } = useContext(HourContext);
+
+    const qty = [' ', 1, 2, 3, 4, 5];
+
+    const [keys, setKeys] = useState();
+    const [hourly, setHourly] = useState();
+    const [foodNutri, setFoodNutri] = useState({});
     const [servSize, setServSize] = useState('');
-    const qty = [0, 1, 2, 3, 4, 5];
+    const [savedData, setSavedData] = useState();
     const [fuelConsumed, setFuelConsumed] = useState({
         time: 0,
         food: '',
-        foodqty: 0,
-        water: 0,
-        tw: 0,
+        servings: 0,
+        water: '0',
+        tailwind: '0',
     });
-    const { time, food, foodqty, water, tw } = fuelConsumed;
-    //////////remember that the qty is multiplied by the serving
-    //so 2 qty of soup, which is 1/4 can means
-    //consuming 1/2 can
+    const { time, food, servings, water, tailwind } = fuelConsumed;
+
+    let h20 = Constants.h20,
+        times = Constants.times,
+        tailwindserv = Constants.tailwindserv,
+        newWater,
+        newTw,
+        newFood;
+
+    const loadHours = async () => {
+        await gethours()
+            .then((data) => {
+                let hours = [];
+                let hourKey = [];
+
+                data.map((hour, i) => {
+                    const key = GetItemId(hour);
+                    hourKey.push(key);
+                    return hourKey;
+                });
+                setKeys(hourKey);
+                data.map((hour, i) => {
+                    hours.push(data[i].data);
+                    return hours;
+                });
+                /*
+                .then((data) => {
+                    let hours = [];
+                    data.map((hour, i) => {
+                        const key = GetItemId(hour);
+                        hours.push({
+                            id: key,
+                            calcium: hour.data.calcium,
+                            calories: hour.data.calories,
+                            food: hour.data.food,
+                            hour: hour.data.hour,
+                            magnesium: hour.data.magnesium,
+                            potassium: hour.data.potassium,
+                            protein: hour.data.protein,
+                            servings: hour.data.servings,
+                            sodium: hour.data.sodium,
+                            tailwindQty: hour.data.tailwindQty,
+                            water: hour.data.water,
+                        });
+                        return hours;
+                    });
+                    */
+                setHours(hours);
+                setHourly(hours[0]);
+            })
+            .catch((err) => {
+                console.log('loadHours API error', err);
+            });
+        setLoading(false);
+    };
+    const loadFood = async () => {
+        await getFuel()
+            .then((fuel) => {
+                let fuelList = [];
+                fuel.map((fuels, i) => {
+                    fuelList.push(fuel[i].data);
+                    return fuelList;
+                });
+                setFuel(fuelList);
+            })
+            .catch((err) => {
+                console.log('loadFood API error', err);
+            });
+        loadHours();
+    };
+
+    useEffect(() => {
+        loadFood();
+        //loadHours();
+    }, []);
+
+    const getHourData = (hr) => {
+        setHourly(hours[hr]);
+    };
+
     const handleLog = (e) => {
         const { name, value } = e.target;
+        if (name === 'time') {
+            let hr = times.indexOf(value);
+            getHourData(hr);
+            setFuelConsumed({ ...fuelConsumed, time: hr });
+            return;
+        }
         setFuelConsumed({ ...fuelConsumed, [name]: value });
     };
 
-    //when a food is selected
     const handleFood = (e) => {
         const { value } = e.target;
         var idx = fuel.filter((obj) => {
             return obj.name.includes(value);
         });
-
         //if food is unselected, remove the serving size
         if (value === '') {
-            //also need to set the food back to an empty string, if a food is selected and then it's changed to the empty option, the food is still saved in fuelConsumed and will prompt user to enter a serving size
-            setFuelConsumed({...fuelConsumed, food: ''})
+            setFuelConsumed({ ...fuelConsumed, food: '' });
+            setFoodNutri({});
             setServSize('');
             return;
         }
+
+        setFoodNutri(idx[0]);
         setServSize(idx[0].serving);
         setFuelConsumed({ ...fuelConsumed, food: value });
     };
@@ -48,229 +145,156 @@ const AddFuel = ({
     //save button
     const handleSave = (e) => {
         e.preventDefault();
-        if (food === '' && water === 0 && tw === 0) {
+        if (food === '' && water === 0 && tailwind === 0) {
             setMessage('Nothing to save');
             return;
         }
-        if (food !== '' && foodqty === 0) {
+
+        if (food !== '' && servings === 0) {
             setMessage('Servings required');
             return;
         }
-        let checkwater = [food, foodqty],
-            liquiddata = {
-                hour: time,
-                water: water,
-                tailwind: tw,
+
+        const isEmpty = (val) => val === '0' || val === '';
+
+        let twdata = {
+            currTw: parseInt(hours[time].tailwindQty),
+            amt: tailwindserv.indexOf(tailwind),
+            data: hourly,
             },
-            data = {
-                hour: time,
-                name: food,
-                qty: foodqty,
-                tailwind: tw,
-                water: water,
+            waterdata = {
+                currWater: parseInt(hours[time].water),
+                water: parseInt(water),
+                data: hourly,
+            },
+            fooddata = {
+                currFood: foodNutri,
+                amt: servings,
+                prevHour: hourly,
             };
 
-        const isEmpty = (val) => val === 0 || val === '';
-
-        if (checkwater.every(isEmpty)) {
-            onlyWater(liquiddata);
-            return;
+        if (!isEmpty(water)) {
+            newWater = WaterHandler(waterdata);
+        }
+        if (!isEmpty(tailwind)) {
+            newTw = TailwindHandler(twdata);
+        }
+        if (!isEmpty(food)) {
+            newFood = FoodHandler(fooddata);
         }
 
-        save(data);
-    };
+        let id = keys[time],
+            contextSave = Object.assign({}, newWater, newTw, newFood),
+            save = Object.assign({}, { time: times[time] }, contextSave);
 
+        addToHour(time, contextSave);
+        setSavedData([save]);
+        edit(id, save);
+        return;
+    };
     return (
         <div className='chart'>
             {/*  MOBILE DISPLAY */}
             <div className='sm-AddFuel'>
-            <fieldset>
-                <div className='drops'>
-                    <label htmlFor='time'>Time</label>
-                    <select
-                        name='time'
-                        id='time'
-                        onChange={handleLog}
-                        className='dropdown'
-                    >
-                        {times.map((clock, idx) => (
-                            <option key={idx} index={idx} value={idx}>
-                                {clock}
-                            </option>
-                        ))}
-                    </select>
-                </div>
-                <div className='drops'>
-                    <label htmlFor='water'>Water</label>
-                    <select
-                        name='water'
-                        id='water'
-                        onChange={handleLog}
-                        className='dropdown'
-                    >
-                        {h20.map((water, idx) => (
-                            <option key={idx} index={idx} value={water}>
-                                {water} ml
-                            </option>
-                        ))}
-                    </select>
-
-                    <label htmlFor='tw'>Tailwind</label>
-                    <select
-                        name='tw'
-                        id='tw'
-                        onChange={handleLog}
-                        className='dropdown'
-                    >
-                        {tailwind.map((tw, idx) => (
-                            <option key={idx} index={idx} value={tw}>
-                                {tw} ml
-                            </option>
-                        ))}
-                    </select>
-                </div>
-                <div className='drops'>
-                    <label htmlFor='food'>Food</label>
-                    <select
-                        name='food'
-                        id='food'
-                        onChange={handleFood}
-                        className='dropdown'
-                    >
-                        {fuel.map((foods, idx) => (
-                            <option
-                                key={idx}
-                                index={idx}
-                                value={foods.name}
-                                name={foods.name}
-                            >
-                                {foods.name}
-                            </option>
-                        ))}
-                    </select>
-                    <label htmlFor='foodqty'>Servings</label>
-                    <select
-                        name='foodqty'
-                        id='foodtqy'
-                        type='number'
-                        onChange={handleLog}
-                        className='dropdown'
-                        required
-                    >
-                        {servSize === '' ? (
-                            <option></option>
-                        ) : (
-                            qty.map((qties, idx) => (
-                                <option key={idx} index={idx} value={qties}>
-                                    {qties}
-                                </option>
-                            ))
-                        )}
-                    </select>
-                </div>
-                <div className='drops'>
-                    <div className='block'>
-                        <option>Serving size: {servSize}</option>
+                <fieldset>
+                    <div className='drops'>
+                        <SelectTime
+                            name={time}
+                            title='Time'
+                            onChange={handleLog}
+                            list={times}
+                        />
                     </div>
-                </div>
+                    <div className='drops'>
+                        <SelectML
+                            name={water}
+                            title='Water'
+                            onChange={handleLog}
+                            list={h20}
+                        />
+                        <SelectML
+                            name={tailwind}
+                            title='Tailwind'
+                            onChange={handleLog}
+                            list={tailwindserv}
+                        />
+                    </div>
+                    <div className='drops'>
+                        <SelectFuel
+                            forlabel='food'
+                            name={food}
+                            id={food}
+                            title='Food'
+                            onChange={handleFood}
+                            list={fuel}
+                        />
+                        <SelectServings
+                            name={servings}
+                            title='Servings'
+                            onChange={handleLog}
+                            list={qty}
+                            servSize={servSize}
+                        />
+                    </div>
+                    <div className='drops'>
+                        <BlockServ servSize={servSize} />
+                    </div>
                 </fieldset>
             </div>
 
             {/* LARGE SCREEN DISPLAY */}
             <div className='lg-AddFuel'>
-            <fieldset>
-            <div className='drops'>
-                <label htmlFor='time'>Time</label>
-                <select
-                    name='time'
-                    id='time'
-                    onChange={handleLog}
-                    className='dropdown'
-                >
-                    {times.map((clock, idx) => (
-                        <option key={idx} index={idx} value={idx}>
-                            {clock}
-                        </option>
-                    ))}
-                </select>
+                <fieldset>
+                    <div className='drops'>
+                        <SelectTime
+                            name={time}
+                            title='Time'
+                            onChange={handleLog}
+                            list={times}
+                        />
+                        <SelectML
+                            name={water}
+                            title='Water'
+                            onChange={handleLog}
+                            list={h20}
+                        />
+                        <SelectML
+                            name={tailwind}
+                            title='Tailwind'
+                            onChange={handleLog}
+                            list={tailwindserv}
+                        />
+                    </div>
+                    <div className='drops'>
+                        <SelectFuel
+                            name={food}
+                            title='Food'
+                            onChange={handleFood}
+                            list={fuel}
+                        />
+                        <BlockServ servSize={servSize} />
+                        <SelectServings
+                            name={servings}
+                            title='Servings'
+                            onChange={handleLog}
+                            list={qty}
+                            servSize={servSize}
+                        />
+                    </div>
+                </fieldset>
+            </div>
 
-                <label htmlFor='water'>Water</label>
-                <select
-                    name='water'
-                    id='water'
-                    onChange={handleLog}
-                    className='dropdown'
-                >
-                    {h20.map((water, idx) => (
-                        <option key={idx} index={idx} value={water}>
-                            {water} ml
-                        </option>
-                    ))}
-                </select>
-
-                <label htmlFor='tw'>Tailwind</label>
-                <select
-                    name='tw'
-                    id='tw'
-                    onChange={handleLog}
-                    className='dropdown'
-                >
-                    {tailwind.map((tw, idx) => (
-                        <option key={idx} index={idx} value={tw}>
-                            {tw} ml
-                        </option>
-                    ))}
-                </select>
-            </div>
-            <div className='drops'>
-                <label htmlFor='food'>Food</label>
-                <select
-                    name='food'
-                    id='food'
-                    onChange={handleFood}
-                    className='dropdown'
-                >
-                {fuel.map((foods, idx) => (
-                        <option
-                            key={idx}
-                            index={idx}
-                            value={foods.name}
-                            name={foods.name}
-                        >
-                            {foods.name}
-                        </option>
-                    )
-                    )}
-                </select>
-                <div className='block'>
-                    <option>Serving size: {servSize}</option>
-                </div>
-                <label hmtlFor='foodqty'>Servings</label>
-                <select
-                    name='foodqty'
-                    id='foodtqy'
-                    type='number'
-                    onChange={handleLog}
-                    className='dropdown'
-                    required
-                >
-                    {servSize === '' ? (
-                        <option></option>
-                    ) : (
-                        qty.map((qties, idx) => (
-                            <option key={idx} index={idx} value={qties}>
-                                {qties}
-                            </option>
-                        ))
-                    )}
-                </select>
-            </div>
-            </fieldset>
-            </div>
             <div className='last'>
-                <button className='save' type='submit' onClick={handleSave}>
+                <button
+                    className='save'
+                    type='submit'
+                    onClick={handleSave}
+                >
                     Save Entry
                 </button>
+                {savedData !== undefined &&
+                    (<Chart data={savedData} handleSave={handleSave} id={keys[time]} />)
+                }
             </div>
         </div>
     );
