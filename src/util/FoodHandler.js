fb5d3d943954df2d0b6c20a369ba9405bc489837
alleycaaat <achulslander@gmail.com>
2023-02-08 12:33:26 -0600

    remove duplicate foods from displaying

--- src/util/FoodHandler.js
+++ src/util/FoodHandler.js
@@ -1,42 +1,41 @@
 const FoodHandler = (fooddata) => {
+    let prevHour = fooddata.prevHour,
+        foodList = prevHour.food,
+        nutriInfo = fooddata.currFood,
+        amt = fooddata.amt;
 
-    let nutriInfo = fooddata.currFood,
-        amt = fooddata.amt,
-        prevHour = fooddata.prevHour;
-    let foodList = prevHour.food;
 
+    //tally the amounts regardless of foodlist
+    for (const key in nutriInfo)
+        if (key in prevHour)
+            prevHour[key] = parseInt(prevHour[key] || 0) + (parseInt(nutriInfo[key]) * amt);
+    //if no food is in hour
     if (prevHour.food === '') {
         prevHour.food = [nutriInfo.name];
         prevHour.servings = amt;
-
-        for (const key in nutriInfo) if (key in prevHour) prevHour[key] = (parseInt(nutriInfo[key]) * amt);
-
         return prevHour;
     } else {
         let tempList = [...foodList, nutriInfo.name],
             findDuplicates = tempList.some((e, i, arr) => arr.indexOf(e) !== i);
+        //if there are duplicate foods
         if (findDuplicates) {
             let getIdx = foodList.indexOf(nutriInfo.name),
                 servIdx = prevHour.servings[getIdx],
                 updatedServings = [...prevHour.servings];
-
-            for (const key in nutriInfo) if (key in prevHour) prevHour[key] = parseInt(prevHour[key]) + (parseInt(nutriInfo[key]) * amt);
 
             updatedServings[getIdx] = parseInt(servIdx) + parseInt(amt);
             prevHour.food = [...prevHour.food];
             prevHour.servings = updatedServings;
 
             return prevHour;
-
+            //if the foods are new to the list
         } else {
             prevHour.food = [...prevHour.food, nutriInfo.name];
             prevHour.servings = [...prevHour.servings, amt];
 
-            for (const key in nutriInfo) if (key in prevHour) prevHour[key] = parseInt(prevHour[key]) + (parseInt(nutriInfo[key]) * amt);
-
             return prevHour;
         }
-    }
+    };
 };
 
 export default FoodHandler;
