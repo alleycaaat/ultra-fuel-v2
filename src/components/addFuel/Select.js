3bd90153fbb1a21751f00088ad81216c296f302f
alleycaaat <achulslander@gmail.com>
2023-02-20 13:18:03 -0600

    still working on labels

--- src/components/addFuel/Select.js
+++ src/components/addFuel/Select.js
@@ -1,22 +1,22 @@
 export const Select = ({ name, title, onChange, list }) => {
     return (
         <span>
-            <label htmlFor={title}>{title}</label>
-            <select
-                name={name}
-                id={title}
-                onChange={onChange}
-                className='dropdown'
-            >
-                {list.map((name, idx) => (
-                    <option
-                        key={idx}
-                        index={idx}
-                        value={name}>
-                        {name} ml
-                    </option>
-                ))}
-            </select>
+            <label htmlFor={title}>{title}
+                <select
+                    name={name}
+                    onChange={onChange}
+                    className='dropdown'
+                >
+                    {list.map((name, idx) => (
+                        <option
+                            key={idx}
+                            index={idx}
+                            value={name}>
+                            {name} ml
+                        </option>
+                    ))}
+                </select>
+            </label>
         </span>
     );
 };
@@ -24,25 +24,25 @@ export const Select = ({ name, title, onChange, list }) => {
 export const SelectFuel = ({ name, title, onChange, list }) => {
     return (
         <span>
-            <label htmlFor={title}>{title}</label>
-            <select
-                name={name}
-                id={title}
-                onChange={onChange}
-                className='dropdown'
-            >
-                {list.map((foods, idx) => (
-                    <option
-                        id={`${ foods.name }`}
-                        key={idx}
-                        index={idx}
-                        value={foods.name}
-                        name={foods.name}
-                    >
-                        {foods.name}
-                    </option>
-                ))}
-            </select>
+            <label>{title}
+                <select
+                    name={name}
+                    onChange={onChange}
+                    className='dropdown'
+                >
+                    {list.map((foods, idx) => (
+                        <option
+                            id={`${ foods.name }`}
+                            key={idx}
+                            index={idx}
+                            value={foods.name}
+                            name={foods.name}
+                        >
+                            {foods.name}
+                        </option>
+                    ))}
+                </select>
+            </label>
         </span>
     );
 };
@@ -50,23 +50,23 @@ export const SelectFuel = ({ name, title, onChange, list }) => {
 export const SelectML = ({ name, title, onChange, list }) => {
     return (
         <span>
-            <label htmlFor={title}>{title}</label>
-            <select
-                name={name}
-                id={title}
-                onChange={onChange}
-                className='dropdown'
-            >
-                {list.map((name, idx) => (
-                    <option
-                        id={`${ name } ml ${ title }`}
-                        key={idx}
-                        index={idx}
-                        value={name}>
-                        {name} ml
-                    </option>
-                ))}
-            </select>
+            <label>{title}
+                <select
+                    name={name}
+                    onChange={onChange}
+                    className='dropdown'
+                >
+                    {list.map((name, idx) => (
+                        <option
+                            id={`${ name } ml ${ title }`}
+                            key={idx}
+                            index={idx}
+                            value={name}>
+                            {name} ml
+                        </option>
+                    ))}
+                </select>
+            </label>
         </span>
     );
 };
@@ -74,23 +74,23 @@ export const SelectML = ({ name, title, onChange, list }) => {
 export const SelectTime = ({ name, title, onChange, list }) => {
     return (
         <span>
-            <label htmlFor='time'>{title}</label>
-            <select
-                name={name}
-                id='time'
-                onChange={onChange}
-                className='dropdown time'
-            >
-                {list.map((name, idx) => (
-                    <option
-                        id={`${ name }`}
-                        key={idx}
-                        index={idx}
-                        value={name}>
-                        {name}
-                    </option>
-                ))}
-            </select>
+            <label>{title}
+                <select
+                    name={name}
+                    onChange={onChange}
+                    className='dropdown time'
+                >
+                    {list.map((name, idx) => (
+                        <option
+                            id={`${ name }`}
+                            key={idx}
+                            index={idx}
+                            value={name}>
+                            {name}
+                        </option>
+                    ))}
+                </select>
+            </label>
         </span>
     );
 };
@@ -98,29 +98,29 @@ export const SelectTime = ({ name, title, onChange, list }) => {
 export const SelectServings = ({ name, title, onChange, servSize, list }) => {
     return (
         <span>
-            <label htmlFor={title}>{title}</label>
-            <select
-                name={name}
-                id={title}
-                type='number'
-                onChange={onChange}
-                className='dropdown serving'
-                required
-            >
-                {servSize === '' ? (
-                    <option>{' '}</option>
-                ) : (
-                    list.map((qties, idx) => (
-                        <option
-                            id={`${ qties } serving`}
-                            key={idx}
-                            index={idx}
-                            value={qties}>
-                            {qties}
-                        </option>
-                    ))
-                )}
-            </select>
+            <label>{title}
+                <select
+                    name={name}
+                    type='number'
+                    onChange={onChange}
+                    className='dropdown serving'
+                    required
+                >
+                    {servSize === '' ? (
+                        <option>{' '}</option>
+                    ) : (
+                        list.map((qties, idx) => (
+                            <option
+                                id={`${ qties } serving`}
+                                key={idx}
+                                index={idx}
+                                value={qties}>
+                                {qties}
+                            </option>
+                        ))
+                    )}
+                </select>
+            </label>
         </span>
     );
 };
