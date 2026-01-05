ed7da57a4c794a27185bedaea42f2e5b39d090fc
AC <82529087+alleycaaat@users.noreply.github.com>
2026-01-04 23:58:53 -0600

    Update index.js

--- src/components/loading/index.js
+++ src/components/loading/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './loading.scss';
-
-export default function Loading() {
-    return (
-        <div id='wrapper'>
+/*
+<div id='wrapper'>
             <span aria-live='polite' className='sr-only'>
                 <p>Content is loading</p>
             </span>
             <div id='loading-text'>LOADING</div>
             <div id='loading-content'></div>
         </div>
+        */
+export default function Loading() {
+    return (
+        
     );
 }
